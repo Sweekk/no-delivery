06101a68df4095d83d219e7bae5105761789b87a
Sweekk <sweekarshetty06@gmail.com>
2026-07-29 15:26:15 +0530

    picker API completed

--- backend/routes/picker.js
+++ backend/routes/picker.js
@@ -5,5 +5,8 @@ const pickerController = require('../controllers/pickerController');
 // Routes for picker actions
 router.get('/active-run', pickerController.getActiveRun);
 router.post('/item-not-found', pickerController.handleNotFound);
+router.get('/order/:order_id', pickerController.getOrderForPicker);
+router.patch('/item/:list_id', pickerController.updateItemStatus);
+router.post('/order/:order_id/complete', pickerController.completeOrder);
 
 module.exports = router;
